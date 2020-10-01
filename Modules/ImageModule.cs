fbe5350de5d7ca45e87b72383ca788cd5d4fef38
Jeff <zhangjeff2014@gmail.com>
2020-09-30 21:59:33 -0300

    added reflection

--- Modules/ImageModule.cs
+++ Modules/ImageModule.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.IO;
 using System.Net;
 using System.Diagnostics;
+using System.Reflection;
 
 namespace LossyBotRewrite
 {
@@ -17,23 +18,33 @@ namespace LossyBotRewrite
         [Command]
         public async Task ImageCommand(params string[] args)
         {
-            ImageEffects effects = new ImageEffects();
             string url = args.Last();
+            Array.Resize(ref args, args.Length - 1); //remove the last element
 
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
+            List<MethodInfo> effects = new List<MethodInfo>();
+            ImageEffects ie = new ImageEffects();
+            Type type = ie.GetType();
+
+            foreach (string effect in args)
+            {
+                effects.Add(type.GetMethod(effect, BindingFlags.IgnoreCase));
+            }
+
             byte[] data = await DownloadImage(url);
 
-            MagickImage img = effects.Magik(data);
+            MagickImage img = new MagickImage(data);
+
+            foreach(var method in effects)
+            {
+                img = (MagickImage)method.Invoke(ie, new MagickImage[] { img });
+            }
+            
             using (var stream = new MemoryStream())
             {
                 img.Write(stream, MagickFormat.Jpg);
                 stream.Position = 0;
                 await Context.Channel.SendFileAsync(stream, "image.jpg");
             }
-            watch.Stop();
-            Console.WriteLine(watch.ElapsedMilliseconds);
-            //img.Write("image.png");
         }
 
         private async Task<byte[]> DownloadImage(string url)
@@ -45,9 +56,8 @@ namespace LossyBotRewrite
 
     public class ImageEffects
     {
-        public MagickImage Magik(byte[] data)
+        public MagickImage Magik(MagickImage image)
         {
-            MagickImage image = new MagickImage(data);
             image.LiquidRescale(new Percentage(65), new Percentage(65));
             image.Resize(new Percentage(135), new Percentage(135));
             return image;
