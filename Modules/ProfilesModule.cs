3bbba54a0594eef8326bae6aecb8ef203b9e9882
JeffDotPng <47371088+JeffDotPng@users.noreply.github.com>
2020-10-02 10:25:31 -0300

    start profiles

--- Modules/ProfilesModule.cs
+++ Modules/ProfilesModule.cs
@@ -2,11 +2,20 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Linq;
 
 namespace LossyBotRewrite
 {
     public class ProfilesModule : ModuleBase<SocketCommandContext>
     {
-
+        
+        
+        private void CheckProfile(string userId)
+        {
+            XDocument doc = XDocument.Load(Globals.path + "profiles.xml");
+            
+            var elements = from p in doc.Root.Descendants("profile") where (p.Attribute("id").Value == userId) select p;
+            if(elements.Any()) return;
+        }
     }
 }
