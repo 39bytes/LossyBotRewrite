9d4f9cb6dcb29800f91ff3d3878d82950992527d
Jeff <zhangjeff2014@gmail.com>
2020-10-12 11:50:19 -0300

    add blank files

---
+++ Modules/SplatnetModule.cs
@@ -0,0 +1,11 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LossyBotRewrite
+{
+    public class SplatnetModule : ModuleBase<SocketCommandContext>
+    {
+    }
+}
