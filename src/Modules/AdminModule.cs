9bac1f52e397e7697a2ccd7384a12432e08a52fd
JeffDotPng <47371088+JeffDotPng@users.noreply.github.com>
2020-10-19 12:23:14 -0300

    toggle color command added

--- src/Modules/AdminModule.cs
+++ src/Modules/AdminModule.cs
@@ -1,11 +1,25 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
+using Discord.Websocket;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LossyBotRewrite
 {
+    [RequireUserPermission(GuildPermission.Administrator)]
     public class AdminModule : ModuleBase<SocketCommandContext>
     {
+        XDocument doc = XDocument.Load(Globals.path + "Servers.xml");
+        [Command("toggle color")]
+        public async Task ToggleCustomColor(bool toggle)
+        {
+            XElement colorElem = doc.Root.XPathSelectElement($"./server[@id='{Context.Guild.Id}']/CustomColor");
+            
+            colorElem.Value = toggle.ToString();
+            await ReplyAsync($"Custom color set to `{toggle}`");
+        }
     }
 }
