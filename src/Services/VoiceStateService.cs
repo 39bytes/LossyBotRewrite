296c50e4a08d154b3d609ae947e082f273acdf62
Jeff <zhangjeff2014@gmail.com>
2020-10-20 18:55:15 -0300

    bot leaves automatically when vc empty

--- src/Services/VoiceStateService.cs
+++ src/Services/VoiceStateService.cs
@@ -25,7 +25,7 @@ namespace LossyBotRewrite
         private async Task OnVoiceStateUpdated(SocketUser user, SocketVoiceState oldState, SocketVoiceState newState)
         {
             XDocument doc = XDocument.Load(Globals.path + "Servers.xml");
-            if (oldState.VoiceChannel == null && newState.VoiceChannel != null)
+            if (oldState.VoiceChannel == null && newState.VoiceChannel != null) //joined voice channel
             {
                 XElement inVoiceChannelElem = doc.Root.XPathSelectElement($"./server[@id='{newState.VoiceChannel.Guild.Id}']/InVoiceChannel");
                 if (inVoiceChannelElem.Value == "")
@@ -34,16 +34,42 @@ namespace LossyBotRewrite
                 OverwritePermissions permissions = new OverwritePermissions(viewChannel: PermValue.Allow, sendMessages: PermValue.Allow);
 
                 var textChannel = newState.VoiceChannel.Guild.GetTextChannel(ulong.Parse(inVoiceChannelElem.Value));
-                await textChannel.AddPermissionOverwriteAsync(user, permissions);
+                await textChannel.AddPermissionOverwriteAsync(user, permissions); //give them invoice channel perms
             }
-            else if(oldState.VoiceChannel != null && newState.VoiceChannel == null)
+            else if(oldState.VoiceChannel != null && newState.VoiceChannel == null) //left voice channel
             {
-                XElement inVoiceChannelElem = doc.Root.XPathSelectElement($"./server[@id='{oldState.VoiceChannel.Guild.Id}']/InVoiceChannel");
+                ulong guild = oldState.VoiceChannel.Guild.Id;
+                if (_voiceManager.HasActiveService(guild))
+                {
+                    if (oldState.VoiceChannel.Id == _voiceManager.GetServiceVoiceChannel(guild).Id)
+                    {
+                        if (oldState.VoiceChannel.Users.Count == 1) //only the bot left in there
+                        {
+                            _voiceManager.ForceStopService(guild); //kill the service
+                        }
+                    }
+                }
+
+                XElement inVoiceChannelElem = doc.Root.XPathSelectElement($"./server[@id='{guild}']/InVoiceChannel");
                 if (inVoiceChannelElem.Value == "")
                     return;
 
                 var textChannel = oldState.VoiceChannel.Guild.GetTextChannel(ulong.Parse(inVoiceChannelElem.Value));
-                await textChannel.RemovePermissionOverwriteAsync(user);
+                await textChannel.RemovePermissionOverwriteAsync(user); //remove their invoice channel perms
+            }
+            else if(oldState.VoiceChannel != null && newState.VoiceChannel != null && oldState.VoiceChannel != newState.VoiceChannel) //moved channel
+            {
+                ulong guild = oldState.VoiceChannel.Guild.Id;
+                if (_voiceManager.HasActiveService(guild))
+                {
+                    if (oldState.VoiceChannel.Id == _voiceManager.GetServiceVoiceChannel(guild).Id)
+                    {
+                        if (oldState.VoiceChannel.Users.Count == 1) //only the bot left in there
+                        {
+                            _voiceManager.ForceStopService(guild); //kill the service
+                        }
+                    }
+                }
             }
         }
     }
