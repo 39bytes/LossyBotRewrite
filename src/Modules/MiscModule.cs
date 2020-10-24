5043cfa81a2e320631be76257cc4e456e9afaf95
Jeff <zhangjeff2014@gmail.com>
2020-10-24 18:27:21 -0300

    add pfp command

---
+++ src/Modules/MiscModule.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LossyBotRewrite
+{
+    public class MiscModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("pfp")]
+        public async Task PfpCommand()
+        {
+            await ReplyAsync(Context.User.GetAvatarUrl());
+        }
+    }
+}
