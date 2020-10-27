df33131be799cc112b69f1c00e30fb19d49da20f
Jeff <zhangjeff2014@gmail.com>
2020-10-27 17:03:23 -0300

    add kill command

--- src/Modules/MiscModule.cs
+++ src/Modules/MiscModule.cs
@@ -1,8 +1,11 @@
 ﻿using Discord.Commands;
+using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using ImageMagick;
+using System.IO;
 
 namespace LossyBotRewrite
 {
@@ -11,7 +14,33 @@ namespace LossyBotRewrite
         [Command("pfp")]
         public async Task PfpCommand()
         {
-            await ReplyAsync(Context.User.GetAvatarUrl());
+            await ReplyAsync(Context.User.GetAvatarUrl(Discord.ImageFormat.Auto, 512));
+        }
+
+        [Command("pfp")]
+        public async Task PfpCommand(SocketGuildUser user)
+        {
+            await ReplyAsync(user.GetAvatarUrl(Discord.ImageFormat.Auto, 512));
+        }
+
+        [Command("kill")]
+        public async Task KillCommand(SocketGuildUser user)
+        {
+            byte[] data = await Globals.httpClient.GetByteArrayAsync(user.GetAvatarUrl(Discord.ImageFormat.Auto, 512));
+            using(MagickImage pfp = new MagickImage(data))
+            {
+                using (MagickImage kill = new MagickImage("kill.png"))
+                {
+                    kill.Resize(pfp.Width, pfp.Height);
+                    pfp.Composite(kill, CompositeOperator.Over);
+                }
+                using(var stream = new MemoryStream())
+                {
+                    pfp.Write(stream);
+                    stream.Position = 0;
+                    await Context.Channel.SendFileAsync(stream, "killed.png");
+                }
+            }
         }
     }
 }
